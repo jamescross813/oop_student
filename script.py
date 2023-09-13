92ae3aa817d2500fa3551628513fced078993a77
jamescross813 <crossjames813@gmail.com>
2023-09-13 10:05:29 -0500

    checks in place

--- script.py
+++ script.py
@@ -6,3 +6,11 @@ class Student:
 
 class Grade:
   pass
+
+roger = Student("Roger van der Weyden", 10)
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
+
+print(roger.name, roger.year)
+print(sandro.name, sandro.year)
+print(pieter.name, pieter.year)
